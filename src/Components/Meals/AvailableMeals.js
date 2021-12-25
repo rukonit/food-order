dd12053e152f2a0d6567e9ae90f5018d3953deaa
rukonit <rukon-it@live.com>
2021-12-25 12:39:05 -0500

    initial upload

---
+++ src/Components/Meals/AvailableMeals.js
@@ -0,0 +1,43 @@
+import dummyMeals from './dummy-meals'
+import styles from './AvailableMeals.module.css'
+
+const AvailableMeals = () => {
+    const DUMMY_MEALS = [
+        {
+          id: 'm1',
+          name: 'Sushi',
+          description: 'Finest fish and veggies',
+          price: 22.99,
+        },
+        {
+          id: 'm2',
+          name: 'Schnitzel',
+          description: 'A german specialty!',
+          price: 16.5,
+        },
+        {
+          id: 'm3',
+          name: 'Barbecue Burger',
+          description: 'American, raw, meaty',
+          price: 12.99,
+        },
+        {
+          id: 'm4',
+          name: 'Green Bowl',
+          description: 'Healthy...and green...',
+          price: 18.99,
+        },
+      ];
+
+    return (
+        <section>
+            <ul>
+                {DUMMY_MEALS.map(meal => 
+                    <li>{meal.name}</li>)}
+                  
+            </ul>
+        </section>
+    )
+}
+
+export default AvailableMeals
