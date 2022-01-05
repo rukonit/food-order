0a5d6d2856417a8ebab103763615b2107f6aa8b0
rukonit <rukon-it@live.com>
2022-01-05 00:43:35 -0500

    Added available meals UX

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -1,5 +1,7 @@
 import dummyMeals from './dummy-meals'
 import styles from './AvailableMeals.module.css'
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
 
 const AvailableMeals = () => {
     const DUMMY_MEALS = [
@@ -29,13 +31,16 @@ const AvailableMeals = () => {
         },
       ];
 
+    const mealList = DUMMY_MEALS.map(meal => <MealItem key ={meal.id} name={meal.name} description={meal.description} price={meal.price} />)
+
     return (
-        <section>
+        <section className={styles.meals}>
+          <Card>            
             <ul>
-                {DUMMY_MEALS.map(meal => 
-                    <li>{meal.name}</li>)}
-                  
+              {mealList}                  
             </ul>
+            </Card>
+
         </section>
     )
 }
