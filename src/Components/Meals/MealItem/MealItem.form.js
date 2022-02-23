a20bafb4d158da8f2d31666cdd3a5793cd4be520
rukonit <rukon-it@live.com>
2022-02-23 10:53:43 -0500

    minor fix

--- src/Components/Meals/MealItem/MealItem.form.js
+++ src/Components/Meals/MealItem/MealItem.form.js
@@ -1,5 +1,4 @@
-import { useRef } from 'react';
-import { useState } from 'react/cjs/react.development';
+import { useRef, useState } from 'react';
 import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
 
